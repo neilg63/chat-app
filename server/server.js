6300741b96bcd521a1fcf09111e275835daab413
Neil Gardner <neilgardner1963@gmail.com>
2016-12-27 19:33:40 +0000

    Integrated socket.io

--- server/server.js
+++ server/server.js
@@ -1,17 +1,32 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
-console.log(publicPath);
+const port = process.env.PORT || 3000;
 
 const app = express();
 
+var server = http.createServer(app);
+
+var io = socketIO(server);
+
 app.use('/', express.static(publicPath));
+
+io.on('connection',(socket) => {
+	console.log("New user connected");
+	socket.on('disconnect',() => {
+		console.log("User was disconnected")
+	});
+});
+
+
 
 /*app.get('/',(req, res) => {
 	res.sendfile(publicPath + '/index.html');
 });*/
 
-app.listen(3000,() => {
-	console.log("Server is up on port 3000");
+server.listen(port,() => {
+	console.log(`Server is up on port ${port}`);
 });
