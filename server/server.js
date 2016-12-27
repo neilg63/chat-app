19b3dbf4e95590f799ecf0c0d0abb454fd08dee3
Neil Gardner <neilgardner1963@gmail.com>
2016-12-27 22:02:38 +0000

    Now emitting messages

--- server/server.js
+++ server/server.js
@@ -16,9 +16,21 @@ app.use('/', express.static(publicPath));
 
 io.on('connection',(socket) => {
 	console.log("New user connected");
-	socket.on('disconnect',() => {
+
+
+	socket.on('createMessage', (message) => {
+		console.log('create message',message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
+	});
+
+		socket.on('disconnect',() => {
 		console.log("User was disconnected")
 	});
+
 });
 
 
